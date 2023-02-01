bbb4ca06c79d65d63188449e73bab308fc5dbe71
jrohitab <101977071+jrohitab@users.noreply.github.com>
2023-01-31 21:25:43 -0800

    Update and rename Factorial.c to Average.c

---
+++ Average.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int n,i;
+	float num[100],sum=0.0,avg;
+	
+	printr\p[;'0']f("Enter the numbers of elements:");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++)
+	{
+		printf("%d.-Enter the number:",i+1);
+		scanf("%f",&num[i]);
+		sum = sum + num[i]; U
+    }
+    
+    avg=sum/n;
+    printf("Average=%.2f",avg);
+    return 0;
+}
